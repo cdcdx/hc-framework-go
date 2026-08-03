3796294b16a9b5fe5a8d00a75c0dc1ff66e9c847
cdcdx <cdcdx888@gmail.com>
2026-08-03 03:46:10 +0000

    k6-test

--- k6-test.sh
+++ k6-test.sh
@@ -4,13 +4,13 @@ make k6-test-idle && sleep 300
 make k6-test-idle-settle && sleep 300
 make k6-test-shop && sleep 300
 make k6-test-shop-flash && sleep 300
-make k6-test-mixed
+make k6-test-mixed && sleep 300
 make k6-test-ws
 
-AUTH_VUS=150                    k6 run scripts/k6/auth.js
-IDLE_VUS=10000 DBSTRESS_VUS=800 k6 run scripts/k6/idle.js
-MAX_VUS=500                     k6 run scripts/k6/idle_settle.js
-SHOP_VUS=1000                   k6 run scripts/k6/shop.js
-FLASH_VUS=1000                  k6 run scripts/k6/shop_flash.js
-MIXED_VUS=2000                  k6 run scripts/k6/mixed.js
-WS_HEARTBEAT_VUS=10000 WS_RECONNECT_VUS=200 k6 run scripts/k6/ws.js
+# AUTH_VUS=150                    k6 run scripts/k6/auth.js
+# IDLE_VUS=10000 DBSTRESS_VUS=800 k6 run scripts/k6/idle.js
+# MAX_VUS=500                     k6 run scripts/k6/idle_settle.js
+# SHOP_VUS=1000                   k6 run scripts/k6/shop.js
+# FLASH_VUS=1000                  k6 run scripts/k6/shop_flash.js
+# MIXED_VUS=2000                  k6 run scripts/k6/mixed.js
+# WS_HEARTBEAT_VUS=10000 WS_RECONNECT_VUS=200 k6 run scripts/k6/ws.js
